ace08710769bfa5dd68ced5a609069c5ef422f6c
stephentuso <tusodev@gmail.com>
2017-02-11 20:18:47 -0500

    Add cloud func

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,34 @@
 
+function addToGroup(skip, limit, groupId) {
+  var groupQuery = new Parse.Query('Group');
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.limit(limit);
+  userQuery.skip(skip);
+  userQuery.include('groups');
+
+  var group;
+  return groupQuery.get(groupId).then(function(g) {
+    group = g;
+    return userQuery.find();
+  })
+  .then(function(u) {
+    for (var i = 0; i < users.length; i++) {
+      var groups = u[i].get('groups') || [];
+      groups.push(group);
+      u[i].set('groups', groups);
+    }
+    return Parse.Object.saveAll(u, { useMasterKey: true });
+  })
+}
+
+Parse.Cloud.define('addToGroup', function(req, res) {
+  Parse.Cloud.useMasterKey();
+  addToGroup(req.params.skip, req.params.limit, req.params.groupId)
+    .then(function() {
+      res.success();
+    })
+})
+
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
