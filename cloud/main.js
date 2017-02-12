4afcdd6c65bf954021a75b513cd2022217003d91
Jacob Merizian <jmerizia@vt.edu>
2017-02-11 23:37:30 -0500

    updated algorithm

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,21 @@
+Parse.Cloud.define('getEvents', function(req, res) {
+    let currentUser = req.user;
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+    // 1. get entire users table <<<<
+    let userQuery = new Parse.Query(Parse.User);
+    userQuery.find().then(function (users) {
+
+    });
+
+    req.user.get("groups")
+    // 3. get every user in each of currentUser's group
+    // 4. intersect groups
+    // 5. choose group with largest intersection with user's groups
+
+    // 6. get list events for each group <<<<
+
+    // 7. compare events to interests and cap to N events /week
+
+    // 8. return list of events (int time order) <<<<
+    res.success('Hi');
 });
